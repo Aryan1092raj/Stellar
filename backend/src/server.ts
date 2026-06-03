0ecc05dea5c389fa48f04a29aec9d7a7db1cee43
Aryan Raj <aryan1092raj@gmail.com>
2026-06-03 09:47:47 +0530

    fixed ci workflows

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ import app from "./app";
 dotenv.config();
 
 const port = Number(process.env.PORT || 4000);
-const host = process.env.HOST || '127.0.0.1';
+const host = process.env.HOST || '0.0.0.0';
 
 app.listen(port, host, () => {
   console.log(`Backend listening on ${host}:${port}`);
